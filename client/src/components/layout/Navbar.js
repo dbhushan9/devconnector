f8b11b80bc920cf80e9cde5b72a2e0a4126fe4e8
Bhushan Deshmukh <dbhushan912@gmail.com>
2019-05-27 15:57:42 +0530

    added Edit profile functionality

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -17,6 +18,9 @@ class Navbar extends Component {
         const {isAuthenticated, user} = this.props.auth;
         const authLinks = (
             <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
                 <li className="nav-item">
                     <a href="" onClick={this.onLogoutClick.bind(this)} className='nav-link'><img className='rounded-circle' src={user.avatar} alt={user.name} style={{width:'25px', marginRight:'5px'}} title='You must have a gravatar connected to your email to display an image' />{' '}Logout</a>
                 </li>
