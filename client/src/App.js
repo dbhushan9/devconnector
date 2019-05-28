f3814c9e0680b54893ef9a8644d5214f241817df
Bhushan Deshmukh <dbhushan912@gmail.com>
2019-05-28 12:16:45 +0530

    add edit education component and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from './components/add-credentials/AddEducation';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
@@ -63,6 +64,9 @@ function App() {
               </Switch>
               <Switch>
                 <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/add-education' component={AddEducation} />
               </Switch>
             </div>
           <Footer />
