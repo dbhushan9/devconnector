31901192d419d3a28dc7f095cbcb9004e1cdd3f3
Bhushan Deshmukh <dbhushan912@gmail.com>
2019-06-03 09:50:08 +0530

    added -- post state and addPost Action

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,15 @@
+
+
+const initialState ={
+    posts: [],
+    post: {},
+    loading: false
+};
+
+export default function(state = initialState, action){
+    switch(action.type){
+
+        default:
+            return state;
+    }
+}
